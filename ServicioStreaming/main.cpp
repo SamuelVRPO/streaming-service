fb8ba053bdd0e09036031dfb8899215a990f3dcf
Samuel Vieira <samuelvieirarpo@gmail.com>
2020-06-03 13:17:31 -0500

    Initial Commit

---
+++ ServicioStreaming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ServicioStreaming
+//
+//  Created by Samuel Vieira on 03/06/20.
+//  Copyright © 2020 Samuel Vieira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
